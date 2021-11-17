91cb09bc4039b983060500845b997bcb9560c10c
Mohammed Kashif <ismekashif@gmail.com>
2021-11-18 02:24:32 +0530

    challenge completed

--- server.js
+++ server.js
@@ -19,13 +19,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// Who am I
+app.get('/api/whoami', (req, res) => {
+  res.json({"ipaddress": req.ip,"language": req.get('Accept-Language'),"software": req.headers['user-agent']});
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
